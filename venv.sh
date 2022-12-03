15df67d4cf1e49dcb3fb0e1447208b51db52bafd
Andy Lee <40545993+ourandy@users.noreply.github.com>
2022-12-03 09:47:47 +0000

    Updated venv.sh

--- venv.sh
+++ venv.sh
@@ -3,14 +3,17 @@
 
 
 #!/bin/bash
-if [ -e ./.venv ]; then
-    echo -e "Activating virtual environment '.venv' ... \n"
-    source "./.venv/Scripts/activate"
-else
-    echo -e "Creating virtual environment '.venv' ... \n"
-    py -m venv .venv
+function activate_venv {
     echo -e "Activating virtual environment '.venv' ... \n"
     source "./.venv/Scripts/activate"
     echo -e "Running pip install on requirements.txt ... \n"
     pip install -r requirements.txt
+}
+
+if [ -e ./.venv ]; then
+    activate_venv
+else
+    echo -e "Creating virtual environment '.venv' ... \n"
+    py -m venv .venv
+    activate_venv
 fi
